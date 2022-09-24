6e4840e11a8e69ee4a9b5453dce1cb50d7065d43
AlejoAlegreBustos <ale21020@byui.edu>
2022-09-24 16:50:55 -0300

    upadates: organization folders and HiLo game completed

---
+++ HiLo game/game/card.py
@@ -0,0 +1,39 @@
+import random
+
+
+# TODO: Implement the Die class as follows...
+
+# 1) Add the class declaration. Use the following class comment.
+"""A small cube with a different number of spots on each of its six sides.
+
+The responsibility of Die is to keep track of the side facing up and calculate the points for 
+it.
+
+Attributes:
+    value (int): The number of spots on the side facing up.
+    points (int): The number of points the die is worth.
+"""
+
+class Card():
+
+        def __init__(self):
+
+            self.current_card=0
+            self.next_card=0
+            self.input_user=""
+            self.points=0
+
+        def random_card(self,guess_user):
+
+            self.current_card=random.randint(1, 7)
+            self.next_card=random.randint(1, 7)
+            self.input_user=guess_user
+
+            if self.next_card < self.current_card and self.input_user=="l" or self.next_card > self.current_card and self.input_user=="h" :
+                self.points=+100
+                return self.next_card
+            elif self.next_card > self.current_card and self.input_user=="l" or self.next_card < self.current_card and self.input_user=="h":
+                self.points=-75
+                return self.next_card
+
+
---
+++ HiLo game/game/director.py
@@ -0,0 +1,75 @@
+from .card import Card
+
+
+class Director:
+
+    def __init__(self):
+        """Constructs a new Director.
+
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.card = 0
+        self.guess_user=""
+        self.is_playing = True
+        self.score = 0
+        self.total_score = 0
+
+
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Ask the user if they want to roll.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        guess_card = input("The next card will be higher o lower? [h/l]:  ")
+        self.guess_user = guess_card
+
+    def do_updates(self):
+        """Updates the player's score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return
+
+        card=Card()
+        self.card=card.random_card(self.guess_user)
+        self.score += card.points
+        self.total_score += self.score
+
+    def do_outputs(self):
+        """Displays the dice and the score. Also asks the player if they want to roll again.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return
+
+        value=self.card
+
+        print(f"You card: {value}")
+        print(f"Your score is: {self.total_score}\n")
+        
+        if self.score > 0:
+            keep_playing= input("Would you like keep playing?[y/n]: ")
+            if keep_playing == "y":
+                self.is_playing = True
+            else:
+                self.is_playing = False
+        else:
+            self.is_playing = False
